b386d5a5ff5da3add0cde0383d342b99d072935d
SantiagoCastilloRivera <142427416+SantiagoCastilloRivera@users.noreply.github.com>
2023-08-30 10:17:12 -0500

    Add files via upload

---
+++ Exercise_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercise_1;
+
+/**
+ *
+ * @author IngSis
+ */
+public class Exercise_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
