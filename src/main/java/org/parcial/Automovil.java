d5358a7972a233d3ffbdc3ec7810ed16d8c72d7f
SantiagoCastilloRivera <142427416+SantiagoCastilloRivera@users.noreply.github.com>
2023-11-17 12:10:56 -0500

    Add files via upload

--- src/main/java/org/parcial/Automovil.java
+++ src/main/java/org/parcial/Automovil.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package org.parcial;
 
-/**
- *
- * @author iikan
- */
 public class Automovil extends Vehiculo{
       private int numeroPuertas;
       private int horaEntrada, horaSalida;
